17605677be63eb237896fa08641a3570649e9ff5
James Hedin <jhedin10@gmail.com>
2026-04-21 11:03:52 -0600

    feat: rate-limiter-tiers

--- solution.test.ts
+++ solution.test.ts
@@ -1,27 +1,58 @@
-import { describe, it } from "node:test";
 import assert from "node:assert/strict";
+import { describe, it } from "node:test";
 // assert.deepStrictEqual(a, b) — deep equality (arrays, objects, ignores key order)
 // assert.strictEqual(a, b)     — primitives (===)
 // assert.ok(value)             — truthy check
 // assert.throws(() => fn())    — expects an error
-import { solution } from "./solution.ts";
+import { RateLimiter } from "./solution.ts";
 
 describe("solution", () => {
-    it("finds two numbers that add up to target", () => {
-        const props = { nums: [2, 7, 11, 15], target: 9 };
-        const expected = [0, 1];
-        assert.deepStrictEqual(solution(props), expected);
+
+    it("limits rates", () => {
+
+        const limiter = RateLimiter({
+            tiers: [
+                { capacity: 2, refillPeriod: 3 },
+                { capacity: 3, refillPeriod: 100 },
+            ]
+        });
+        console.log("u1", 0)
+        assert.ok(limiter.allowRequest("u1", 0));   // true,  tokens: 1, 3                                                                                                                                       
+        console.log("u1", 1)
+        assert.ok(limiter.allowRequest("u1", 1));   // true,  tokens: 0, 2                                                                                                                                        
+        console.log("u1", 2)
+        assert.ok(!limiter.allowRequest("u1", 2));   // false,  no tokens                                                                                                                                         
+        console.log("u1", 4)
+        assert.ok(limiter.allowRequest("u1", 4));   // true, tokens 0, 1                                                                                                                                  
+        console.log("u1", 50)
+        assert.ok(!limiter.allowRequest("u1", 50));  // false,  tokens, 0, 0
+        console.log("u1", 100)
+        assert.ok(limiter.allowRequest("u1", 100));  // true,  refill happened
+
+
     });
 
-    it("works when answer is not at the start", () => {
-        const props = { nums: [3, 2, 4], target: 6 };
-        const expected = [1, 2];
-        assert.deepStrictEqual(solution(props), expected);
+    it(" no limits when no tiers", () => {
+
+        const limiter = RateLimiter({
+            tiers: [
+            ]
+        });
+        console.log("u1", 0)
+        assert.ok(limiter.allowRequest("u1", 0));   // true,  tokens: 1, 3                                                                                                                                       
+        console.log("u1", 1)
+        assert.ok(limiter.allowRequest("u1", 1));   // true,  tokens: 0, 2                                                                                                                                        
+        console.log("u1", 2)
+        assert.ok(limiter.allowRequest("u1", 2));   // false,  no tokens                                                                                                                                         
+        console.log("u1", 4)
+        assert.ok(limiter.allowRequest("u1", 4));   // true, tokens 0, 1                                                                                                                                  
+        console.log("u1", 50)
+        assert.ok(limiter.allowRequest("u1", 50));  // false,  tokens, 0, 0
+        console.log("u1", 100)
+        assert.ok(limiter.allowRequest("u1", 100));  // true,  refill happened
+
+
     });
 
-    it("handles duplicate values", () => {
-        const props = { nums: [3, 3], target: 6 };
-        const expected = [0, 1];
-        assert.deepStrictEqual(solution(props), expected);
-    });
+
 });
--- solution.ts
+++ solution.ts
@@ -1,20 +1,44 @@
 
-
-interface Props {
-    nums: number[];
-    target: number;
+interface Tier {
+    capacity: number
+    refillPeriod: number
 }
 
-type Result = [number, number];
+interface Constructor {
+    tiers: Tier[]
+}
 
-export function solution({ nums, target }: Props): Result {
-    const seen = new Map<number, number>();
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (seen.has(complement)) {
-            return [seen.get(complement)!, i];
+export function RateLimiter({ tiers }: Constructor) {
+
+    let ttl = tiers.reduce((max, next) => Math.max(max, next.refillPeriod), 0)
+
+    const userBuckets: Record<string, number[]> = {}
+
+    function checkTier(userTokens: number[], timestamp: number, { capacity, refillPeriod }: Tier) {
+        let tierUserTokens = userTokens.filter(requestTime => timestamp - requestTime < refillPeriod)
+        console.log(timestamp, capacity, refillPeriod)
+        console.table(tierUserTokens)
+        if (tierUserTokens.length < capacity) {
+            return true
         }
-        seen.set(nums[i], i);
+        return false
     }
-    throw new Error("No solution found");
+
+    return {
+        allowRequest: (user: string, timestamp: number): boolean => {
+            console.table(userBuckets)
+
+            let userTokens = (userBuckets[user] ?? []).filter(requestTime => timestamp - requestTime < ttl)
+
+            let allowed = tiers.every(tier => checkTier(userTokens, timestamp, tier))
+
+            if (allowed) {
+                userTokens.push(timestamp)
+            }
+
+            userBuckets[user] = userTokens
+
+            return allowed
+        }
+    }
 }
